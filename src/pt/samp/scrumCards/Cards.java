163d7e8756835061ccab94332ee2d36bf57dc78b
Sérgio Patrício <spatricio@gmail.com>
2012-01-09 21:15:40 +0000

    code clean

--- src/pt/samp/scrumCards/Cards.java
+++ src/pt/samp/scrumCards/Cards.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 public final class Cards {
     public static final int[] IDS = new int[] { R.id.card0, R.id.card0_5, R.id.card1, R.id.card2, R.id.card3,
             R.id.card5, R.id.card8, R.id.card13, R.id.card20, R.id.card40, R.id.card100, R.id.cardq };
@@ -14,9 +13,8 @@ public final class Cards {
             "?" };
 
     public static TextView createCardView(Context context, ViewGroup parent, String cardValue) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService (Context.LAYOUT_INFLATER_SERVICE);
-        TextView textView =  (TextView) inflater.inflate(R.layout.card, parent, false);
-        //textView.setLayoutParams(new android.widget.Gallery.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        TextView textView = (TextView) inflater.inflate(R.layout.card, parent, false);
         textView.setText(cardValue);
         return textView;
     }
